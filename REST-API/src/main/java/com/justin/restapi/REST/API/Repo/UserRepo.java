02d67a9b5dae6add5dd8cd6a4ce9f990e33667e4
Justin Garner <j.garner828@gmail.com>
2022-06-20 08:37:52 -0400

    Delete REST-API directory

--- REST-API/src/main/java/com/justin/restapi/REST/API/Repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.justin.restapi.REST.API.Repo;
-
-import com.justin.restapi.REST.API.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
