f82b709f5f3f7e6be11fad0cb88358a3012afa68
Justin Garner <j.garner828@gmail.com>
2022-06-20 16:11:27 -0400

    finished basic CRUD operations for a table with minimal requirements. next step is looking on how to keep passwords encrypted and checking authentication

---
+++ readme.md
@@ -0,0 +1,12 @@
+# REST API for MySQL
+
+## Author
+Justin Garner
+
+
+
+## Details
+This is just a simple example of a REST API for MySQL Databases that supports basic CRUD operations. This project will grow as I learn about better processes for using Java as an API. Watch for more soon!
+
+# Technical Documentation
+Built using Java, Spring Boot, JPA, and MYSQL Server. Maven for dependencies.
--- src/main/java/com/justin/RESTAPI/Java/REST/API/Controllers/ApiControllers.java
+++ src/main/java/com/justin/RESTAPI/Java/REST/API/Controllers/ApiControllers.java
@@ -1,12 +1,55 @@
 package com.justin.RESTAPI.Java.REST.API.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.justin.RESTAPI.Java.REST.API.Models.User;
+import com.justin.RESTAPI.Java.REST.API.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiControllers {
 
-@RequestMapping(value = "/", method = RequestMethod.GET)
-public String getMain(){ return "Hello main!"; }
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getMain(){ return "Hello main!"; }
+
+//add user to db
+    @PostMapping(value = "/adduser")
+    public User addUser(@RequestBody User user) {
+        User newUser = userRepo.save(user);
+    return newUser;
+    }
+
+//    TODO find users
+    @RequestMapping(value = "/users")
+    public List<User> getUsers() {
+        return   userRepo.findAll();
+    }
+
+//  TODO update users by id
+    @PutMapping(value = "/update/{id}")
+    public User updateUser(@PathVariable long id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setUsername(user.getUsername());
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setPassword(user.getPassword());
+        userRepo.save(updatedUser);
+        return updatedUser;
+
+    }
+
+//  TODO  Delete user by id
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUserById(@PathVariable Long id) {
+        userRepo.deleteById(id);
+        return "User deleted successfully";
+    }
+
+
+
 }
